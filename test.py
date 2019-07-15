a85b41c358042c918117ab00733c751d7f64fcf9
root <root@tf-trainer2.raffle-ai.cloud>
2019-07-15 11:46:40 +0000

    data augmentation

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from sql.syntaxsql import SyntaxSQL
 from utils.dataloader import SpiderDataset
 from embedding.embeddings import GloveEmbedding
 emb = GloveEmbedding(path='data/'+'glove.6B.300d.txt')
-spider = SpiderDataset(data_path='data/dev.json', tables_path='data/tables.json',exclude_keywords=["between", "distinct", '-', ' / ', ' + ']) 
+spider = SpiderDataset(data_path='data/dev.json', tables_path='data/tables.json',exclude_keywords=['-', ' / ', ' + ']) 
 syntax_sql = SyntaxSQL(embeddings=emb, N_word=emb.embedding_dim, hidden_dim=100, num_layers=2, gpu=True)
 
 corrects = 0
